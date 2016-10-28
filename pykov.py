7caef4a4d2b0d2a013eee247234c9577a229c98e
Eric Brovont <Eric Brovont>
2016-10-28 12:40:16 -0400

    Added route to login page

--- pykov.py
+++ pykov.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template("index.html")
+@app.route('/login.html')
+def login():
+	return render_template("login.html")	
 
 if __name__ == '__main__':
 	app.run('', 4999)
