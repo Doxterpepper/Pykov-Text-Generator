e0feef6e807c64b908f5d3b081aea79f62fdbd83
Deathande <dockconnellyoneal@gmail.com>
2016-10-31 14:16:41 -0400

    Added the api to generate text based on a corpus sent with the request.

--- pykov.py
+++ pykov.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import db_init
+import Markov
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/api/gen')
+def userless_gen():
+	data = request.get_json()
+	return Markov.gen(data['text'], data['n'])
+
+@app.route('/test')
+def test():
+	print(request.args.get('this'))
+	return 'test'
+	
 
 if __name__ == '__main__':
 	app.run('', 4999)
