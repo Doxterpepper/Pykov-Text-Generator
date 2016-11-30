71d7cec1d48b5f331262f15c149365110b544d5b
JMadahAmiri <wizzlepants@gmail.com>
2016-11-30 17:55:19 -0500

    Add files via upload

--- static/rest_client.js
+++ static/rest_client.js
@@ -10,7 +10,7 @@ function get_corpus(id, token) {
 	requests.open("POST", url+"/api/corpus", false);
 	var data = JSON.stringify({"id": id, 'token': token});
 	ret = requests.send(data);
-	var text = JSON.parse(requests.response);
+	var response = JSON.parse(requests.response);
 	document.getElementById('corpus').value = text['text'];
 	document.getElementById('title').value = text['title'];
 }
@@ -18,6 +18,17 @@ function get_corpus(id, token) {
 function generate() {
 }
 
-function save_corpus() {
-	
+function save_corpus(token) {
+	var requests =new XMLHttpRequest();
+	requests.open("POST", url+"/api/upload", false);
+	corpus = document.getElementById('corpus').value;
+	title = document.getElementById('title').value;
+	if (corpus == "" || title == "")
+	{
+		error = "Please input text into both the title, and corpus fields."
+		alert(error);
+		return error;
+	}
+		var data = JSON.stringify({'token': token, 'title': title, 'corpus':corpus});
+		ret = requests.send(data);
 }
