ef44b6eb1930c0c6894dc3854ccc732c4932db40
JMadahAmiri <wizzlepants@gmail.com>
2016-11-30 17:49:14 -0500

    Add files via upload
    Initial upload functionality

---
+++ rest_client.js
@@ -0,0 +1,34 @@
+var url="http://localhost:4999"
+
+function clear_corpus() {
+	document.getElementById('corpus').value = "";
+	document.getElementById('title').value = "";
+}
+
+function get_corpus(id, token) {
+	var requests = new XMLHttpRequest();
+	requests.open("POST", url+"/api/corpus", false);
+	var data = JSON.stringify({"id": id, 'token': token});
+	ret = requests.send(data);
+	var response = JSON.parse(requests.response);
+	document.getElementById('corpus').value = text['text'];
+	document.getElementById('title').value = text['title'];
+}
+
+function generate() {
+}
+
+function save_corpus(token) {
+	var requests =new XMLHttpRequest();
+	requests.open("POST", url+"/api/upload", false);
+	corpus = document.getElementById('corpus').value;
+	title = document.getElementById('title').value;
+	if (corpus == "" || title == "")
+	{
+		error = "Please input text into both the title, and corpus fields."
+		alert(error);
+		return error;
+	}
+		var data = JSON.stringify({'token': token, 'title': title, 'corpus':corpus});
+		ret = requests.send(data);
+}
