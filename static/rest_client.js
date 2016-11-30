343974efa39d38dbdd8a0336fb2051ed90b54ef8
Deathande <dockconnellyoneal@gmail.com>
2016-11-30 16:50:18 -0500

    Javascript to access the generation API.

--- static/rest_client.js
+++ static/rest_client.js
@@ -16,6 +16,25 @@ function get_corpus(id, token) {
 }
 
 function generate() {
+	var requests = new XMLHttpRequest();
+	requests.open("POST", url+"/api/gen", false);
+	var text = document.getElementById('corpus').value;
+	var num = document.getElementById('controls').childNodes[1].value;
+
+	num = parseInt(num, 10);
+	//console.log(typeof(num));
+	if (isNaN(num)) {
+		alert("Invalid input");
+		return;
+	}
+	var data = JSON.stringify({
+		"corpus": text,
+		"n": num
+	});
+	requests.send(data);
+	var response = JSON.parse(requests.response);
+	console.log(response["corpus"]);
+	document.getElementById('generated').childNodes[1].innerText = response["corpus"];
 }
 
 function save_corpus() {
